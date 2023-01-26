a7658c0a0a4b511c9d88fd1f8fd5d69dcaf84a3f
Paul Chesa <paulchesa1@gmail.com>
2023-01-26 06:24:10 +0300

    :tada: Created artists features library

---
+++ libs/features/artists/src/lib/artists.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-artists',
+  template: `
+    <p>
+      artists works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ArtistsComponent {
+
+}
---
+++ libs/features/artists/src/lib/artists.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ArtistsComponent } from './artists.component';
+
+
+
+@NgModule({
+  declarations: [
+    ArtistsComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ArtistsComponent
+  ]
+})
+export class ArtistsModule { }
