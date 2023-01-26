5ace37c37cd1f7febca557eb29b870a43f349f4c
Paul Chesa <paulchesa1@gmail.com>
2023-01-26 07:00:11 +0300

    Created artist model

---
+++ libs/model/artists/src/lib/artist.interface.ts
@@ -0,0 +1,18 @@
+/**
+ * @Ref  https://developers.deezer.com/api/artist
+ */
+export interface IArtist {
+  id: number;
+  name: string;
+  link: string;
+  share: string;
+  picture: string;
+  picture_small: string;
+  picture_medium: string;
+  picture_big: string;
+  picture_xl:string;
+  nb_album:number;
+  nb_fan:number;
+  radio:boolean;
+  tracklist:string;
+}
---
+++ libs/model/artists/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of artists
+ */
+
+export * from './lib/artists.service';
+export * from './lib/artists.component';
+export * from './lib/artists.module';
