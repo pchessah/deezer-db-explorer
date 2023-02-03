7491acc71453bdf09baf8dfdec95201cad6ba3c2
Paul Chesa <paulchesa1@gmail.com>
2023-02-03 10:50:15 +0300

    use debugger to handle prod

--- libs/features/artists/src/lib/components/search-bar/search-bar.component.ts
+++ libs/features/artists/src/lib/components/search-bar/search-bar.component.ts
@@ -28,6 +28,7 @@ export class SearchBarComponent {
       return;
     } else {
       return this._searchArtistService.searchArtist(artist).pipe(filter(artistData => !!artistData)).subscribe(artistData => {
+        debugger
         this.artistDetails = artistData.map((s: { artist: any; }) => s.artist);
         this.searchArtistEvent.emit(this.artistDetails);
         this.isLoaded = true;
